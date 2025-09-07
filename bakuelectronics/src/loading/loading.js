63bc12db284f8444e0f10549db90208bd18f92dc
LAMIYA ALIZADA <lamiyaea@code.edu.az>
2025-09-07 15:21:40 +0400

    loading

--- bakuelectronics/src/loading/loading.js
+++ bakuelectronics/src/loading/loading.js
@@ -1,9 +1,9 @@
 const loading = () => {
-    const LOADER = document.querySelector("#Loading");
-    setTimeout(() => {
-      LOADER && LOADER.classList.add("hidden");
-      LOADER && LOADER.classList.remove("flex");
-    }, 1000);
-  };
-  
-  export default loading;
+  const LOADER = document.querySelector("#Loading");
+  setTimeout(() => {
+    LOADER && LOADER.classList.add("hidden");
+    LOADER && LOADER.classList.remove("flex");
+  }, 2000);
+};
+
+export default loading;
